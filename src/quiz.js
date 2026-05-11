37f167648695668b2ca20f230dd3340a16b4c915
yemi <o.ogunbadejo7@gmail.com>
2026-05-11 15:08:21 +0100

    created the quiz class

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,41 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questions, timeLimit, timeRemaining){
+        this.questions = questions; //array
+        this.timeLimit = timeLimit; //number
+        this.timeRemaining = timeRemaining; //number
+        this.correctAnswers = 0; //
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
-    
+        getQuestion(){
+            return this.questions[this.currentQuestionIndex];
+        }
     // 3. moveToNextQuestion()
-
+        moveToNextQuestion(){
+            this.currentQuestionIndex++
+        }
     // 4. shuffleQuestions()
+        shuffleQuestions(){
+        for (let i=0; i<this.questions.length; i++){
+            const j = Math.floor(Math.random()* (i+1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]
+        }
+        return this.questions
+    }
 
     // 5. checkAnswer(answer)
-
+        checkAnswer(answer){
+            this.correctAnswers++
+        }
+    
     // 6. hasEnded()
-}
+        hasEnded(){
+            if (this.currentQuestionIndex<this.questions.length){
+                return false;
+            } else{
+                return true;
+            }
+        }
+    }
